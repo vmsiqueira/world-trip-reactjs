8665a94bbe46de58feaebd26af5af278b4325dfc
Vitor Siqueira <vitormsiqueira@hotmail.com>
2022-07-15 18:56:58 -0300

    swipper correction

--- src/components/Swiper/SwiperItem.tsx
+++ src/components/Swiper/SwiperItem.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, Text, Image } from "@chakra-ui/react";
+import Link from "next/link";
 
 interface ContinentProps {
     title: string;
@@ -8,29 +9,35 @@ interface ContinentProps {
 
 export function SwiperItem({ title, abstract, backgroundImg }: ContinentProps){
   return (
-    <Box 
-      mt='52px' 
-      bg='pink.100' 
-      maxW='1120px' 
-      h='450px' 
-      bgImage={backgroundImg}
-      mx='auto'
-      mb='10'
-      display='flex'
-      flexDirection='column'
-      justifyContent='center'
-      alignItems='center'
-      position='relative'
-    >
-      <>
-          <Box w='full' h='full' position='absolute' bgColor='blackAlpha.500' />
-          <Heading zIndex='1' color='brand.white' size='2xl' fontWeight='bold'>
-            {title}
-          </Heading>
-          <Text zIndex='1' mt='4' fontSize='sm' fontWeight='bold' color='brand.white'>
-            {abstract}
-          </Text>
-        </>
-    </Box>
+    <Link href={`/${title.toLocaleLowerCase()}`}>
+      <a>
+        <Box 
+          mt='52px' 
+          bg='pink.100' 
+          maxW='1120px' 
+          h='450px' 
+          bgImage={backgroundImg}
+          bgSize='cover'
+          bgRepeat='no-repeat'
+          mx='auto'
+          mb='10'
+          display='flex'
+          flexDirection='column'
+          justifyContent='center'
+          alignItems='center'
+          position='relative'
+        >
+          <>
+            <Box w='full' h='full' position='absolute' bgColor='blackAlpha.500' />
+              <Heading zIndex='1' color='brand.white' size='2xl' fontWeight='bold'>
+                {title}
+              </Heading>
+              <Text zIndex='1' maxW='2xl' mt='4' fontSize='sm' fontWeight='bold' color='brand.white' align='center'>
+                {abstract}
+              </Text>
+          </>
+        </Box>
+      </a>
+    </Link>
   );
 }
--- src/components/Swiper/index.tsx
+++ src/components/Swiper/index.tsx
@@ -1,14 +1,15 @@
+import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Keyboard } from 'swiper';
 import { Container } from '@chakra-ui/react';
+import Link from 'next/link';
 
 import { SwiperItem } from "./SwiperItem";
+import { api } from '../../services/api';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
 
 interface ContinentsProps {
   id: number;
@@ -36,19 +37,29 @@ export function SwiperBanner() {
         onSlideChange={() => console.log('slideChange')}
         onSwiper={(swiper) => console.log('swiper')}
       >
-        <SwiperSlide>
-          {continentItem.map(continent => {
-            return (
-              <SwiperItem 
-                key={continent.id} 
-                title={continent.title} 
-                abstract={continent.abstract}
-                backgroundImg={continent.backgroundImage}
-              />
-            )
-          })}
-        </SwiperSlide>
+        { continentItem.map(continent => {
+          return( 
+            <SwiperSlide key={continent.id}>
+                <SwiperItem 
+                  title={continent.title} 
+                  abstract={continent.abstract}
+                  backgroundImg={continent.backgroundImage}
+                /> 
+            </SwiperSlide>         
+          );
+        }) }
       </Swiper>
     </Container>
   );
 }
+
+// {continentItem.map(continent => {
+//   return (
+//     <SwiperItem 
+//       key={continent.id} 
+//       title={continent.title} 
+//       abstract={continent.abstract}
+//       backgroundImg={continent.backgroundImage}
+//     />
+//   )
+// })}
